35c0f1161b68b1c66f13362f1f3ef8a1c89b8fb2
ShafinRME <shafin.rmedu@gmail.com>
2025-12-25 22:54:36 +0600

    Mongodb connection and POST AND GET api making is done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Load environment variables from .env file
 dotenv.config();
+
+// e-commerce-1
+// 1GQyAfP13zwpN5O3
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +15,70 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbd6wjw.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db('parcelDB'); // database name
+        const parcelCollection = db.collection('parcels'); // collection
+
+        app.get('/parcels', async (req, res) => {
+            const parcels = await parcelCollection.find().toArray();
+            res.send(parcels);
+        });
+
+        // parcels api
+        // GET: All parcels OR parcels by user (created_by), sorted by latest
+        app.get('/parcels', async (req, res) => {
+            try {
+                const userEmail = req.query.email;
+
+                const query = userEmail ? { created_by: userEmail } : {};
+                const options = {
+                    sort: { createdAt: -1 }, // Newest first
+                };
+
+                const parcels = await parcelCollection.find(query, options).toArray();
+                res.send(parcels);
+            } catch (error) {
+                console.error('Error fetching parcels:', error);
+                res.status(500).send({ message: 'Failed to get parcels' });
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 // Sample route
