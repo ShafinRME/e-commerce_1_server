ddb3025db68292ab85eaff04d6d71aac48221ea5
ShafinRME <shafin.rmedu@gmail.com>
2025-12-28 16:54:06 +0600

    Pending riders api is done

--- index.js
+++ index.js
@@ -176,6 +176,29 @@ async function run() {
         });
 
 
+        app.patch("/riders/:id/status", async (req, res) => {
+            const { id } = req.params;
+            const { status } = req.body;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set:
+                {
+                    status
+                }
+            }
+
+            try {
+                const result = await ridersCollection.updateOne(
+                    query, updateDoc
+
+                );
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ message: "Failed to update rider status" });
+            }
+        });
+
+
         // Tracking API
 
         app.post("/tracking", async (req, res) => {
